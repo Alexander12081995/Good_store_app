210e27751848c0be05aa3bfa99dad56a204ffc97
Alexander12081995 <sgen28@gmail.com>
2023-03-12 08:37:17 +0300

    fix bags

--- src/components/Common/Slider/Slider.tsx
+++ src/components/Common/Slider/Slider.tsx
@@ -3,12 +3,13 @@ import {Button, Carousel} from "antd";
 import {Link} from "react-router-dom";
 import categories from "../../../api/categories.json";
 import main2 from "../../../images/main2.jpg";
+import main3 from "../../../images/main3.jpg";
 import css from './carousel.module.css';
 
 export const CarouselImg = () => {
     return(
         <div>
-            <Carousel autoplay dots={false}>
+            <Carousel autoplay dots={true}>
                 <div className={css.carousel}>
                     <img src={main1} alt={'books'} className={css.img}/>
                     <h2 className={css.title}>Книги со скидкой 20%</h2>
@@ -20,6 +21,20 @@ export const CarouselImg = () => {
                     <Button className={css.btn2}><Link to={`/categories/${categories[6].type}`}>Заказать</Link></Button>
                 </div>
             </Carousel>
+        </div>
+    )
+}
+
+const dataSlider = [
+    {id: "1", img: main1, title: "Книги со скидкой 20%", btn: "Выбрать книги"},
+    {id: "2", img: main2, title: "Самые весенние книги", btn: "Выбрать книги"},
+    {id: "3", img: main3, title: "Топ-1000 книг со скидками до 40%", btn: "Выбрать книги"}
+]
+
+export const Slider = () => {
+    return (
+        <div>
+
         </div>
     )
 }
